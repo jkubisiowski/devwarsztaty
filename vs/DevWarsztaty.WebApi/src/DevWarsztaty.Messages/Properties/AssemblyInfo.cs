81b7afcb32214dabb78d671b30ac5bce180eba8a
Jakub Kubisiowski <jkubisiowski@abis.krakow.pl>
2017-06-03 09:56:32 +0200

    Added 3 projects

---
+++ vs/DevWarsztaty.WebApi/src/DevWarsztaty.Messages/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevWarsztaty.Messages")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e4463920-6f8a-4a9c-8694-3104f8e29cb8")]
